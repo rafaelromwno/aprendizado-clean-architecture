0904d7cb1b3a153f110994db728950d789462145
rafaelromwno <romanosilvarafael@gmail.com>
2025-04-29 14:52:36 -0300

    feat: implementando a inversão de controle

---
+++ Catalogo/Catalogo.CrossCutting/IoC/DependencyInjectionAPI.cs
@@ -0,0 +1,30 @@
+﻿using Catalogo.Application.Interfaces;
+using Catalogo.Application.Mappings;
+using Catalogo.Application.Services;
+using Catalogo.Domain.Interfaces;
+using Catalogo.Infrastructure.Context;
+using Catalogo.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalogo.CrossCutting.IoC;
+
+public static class DependencyInjectionAPI
+{
+    public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+           options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+        services.AddScoped<IProdutoRepository, ProdutoRepository>();
+        services.AddScoped<IProdutoService, ProdutoService>();
+        services.AddScoped<ICategoriaService, CategoriaService>();
+
+        services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+        return services;
+    }
+}
